c2b52d81cea1abdada8a699f2e627d9953066fbd
taktik <taktik@renoise.com>
2025-09-28 14:00:14 +0200

    update examples

--- examples/stream-file.rs
+++ examples/stream-file.rs
@@ -1,5 +1,4 @@
 use std::error::Error;
-use wavers::{write, Wav};
 
 use arg::{parse_args, Args};
 
@@ -13,10 +12,12 @@ struct Arguments {
     /// The stretch speed in range (0 - 100]
     speed: f64,
     #[arg(short = "p", long = "pitch", default_value = "1.0")]
-    /// The resampling pitch
+    /// The resampling pitch in range (0 - 100]
     pitch: f64,
-    /// Input and output file paths
-    paths: Vec<String>,
+    /// Input file path
+    input_path: String,
+    /// Output file path
+    output_path: String,
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -25,52 +26,67 @@ struct Arguments {
 fn main() -> Result<(), Box<dyn Error>> {
     // Parse cmd arguments
     let args = parse_args::<Arguments>();
-    let input_path: String = args
-        .paths
-        .first()
-        .expect("Please specify an input and output path as argument")
-        .to_string();
-    let output_path: String = args
-        .paths
-        .get(1)
-        .expect("Please specify an input and output path as argument")
-        .to_string();
-    let speed: f64 = args.speed;
-    let pitch: f64 = args.pitch;
 
-    // Calculate needed block sizes
-    let input_block_size: usize = 1024;
-    let output_block_size_f = input_block_size as f64 / speed;
-    let output_block_size = output_block_size_f.ceil() as usize;
+    let input_path: String = args.input_path;
+    if input_path.is_empty() {
+        return Err("Please specify an input path as first argument".into());
+    };
+    let output_path: String = args.output_path;
+    if output_path.is_empty() {
+        return Err("Please specify an output path as second argument".into());
+    };
+    let speed: f64 = args.speed;
+    if !(0.0..=100.0).contains(&speed) {
+        return Err("Invalid speed argument".into());
+    }
+    let pitch: f64 = args.pitch;
+    if !(0.0..=100.0).contains(&pitch) {
+        return Err("Invalid pitch argument".into());
+    }
 
     println!("Converting `{input_path}` -> `{output_path}`");
     println!("Speed: {speed:.2}x, Pitch: {pitch:.2}x");
 
     // Open Wav input file
-    let mut reader = Wav::<f32>::from_path(&input_path)?;
-    let mut wav_output_samples: Vec<i16> = Vec::new(); // interleaved
-    let num_channels = reader.n_channels() as usize;
-    let sample_rate = reader.sample_rate() as usize;
+    let mut wav_reader = wavers::Wav::<f32>::from_path(&input_path)?;
+    let num_channels = wav_reader.n_channels() as usize;
+    let sample_rate = wav_reader.sample_rate() as usize;
 
-    // Prepare stretcher and stream
+    let mut wav_output_samples: Vec<i16> = Vec::new(); // interleaved
+    let mut wav_write = {
+        let output_samples = &mut wav_output_samples;
+        move |planar_output: &Vec<Vec<f32>>, frame_offset: usize, frame_count: usize| {
+            for frame in frame_offset..frame_count {
+                for channel in 0..num_channels {
+                    let sample = planar_output[channel][frame];
+                    output_samples.push((sample * 32767.0) as i16);
+                }
+            }
+        }
+    };
+
+    // Calculate needed block sizes
+    let input_block_size: usize = 1024;
+    let output_block_size = (input_block_size as f64 / speed).ceil() as usize;
+
+    // Create stretcher stream
     let mut stream = Stream::new(sample_rate, num_channels, input_block_size)?;
 
-    // Prepare temporary stream buffers
-    let mut input_deinterleaved = vec![vec![0.0f32; input_block_size]; num_channels];
-    let mut output_deinterleaved = vec![vec![0.0f32; output_block_size]; num_channels];
+    // Prepare temporary stream buffers (planar)
+    let mut input_stream_buffer = vec![vec![0.0f32; input_block_size]; num_channels];
+    let mut output_stream_buffer = vec![vec![0.0f32; output_block_size]; num_channels];
 
     // Process
-    let mut frames_iter = reader.frames();
-    let mut latency_frames = None;
-    let mut total_frames_read = 0;
+    let mut wav_reader_frames = wav_reader.frames();
+    let mut remaining_latency_frames = None;
 
     loop {
         // read interleaved frames into planar input buffer
         let mut frames_read = 0;
-        for i in 0..input_block_size {
-            if let Some(frame) = frames_iter.next() {
-                for ch in 0..num_channels {
-                    input_deinterleaved[ch][i] = frame[ch];
+        for frame in 0..input_block_size {
+            if let Some(samples) = wav_reader_frames.next() {
+                for channel in 0..num_channels {
+                    input_stream_buffer[channel][frame] = samples[channel];
                 }
                 frames_read += 1;
             } else {
@@ -80,33 +96,27 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         if frames_read > 0 {
             // process
-            let output_expected = frames_read as f64 / speed;
-            let output_frame_count = stream.process(
-                Some(&input_deinterleaved),
-                &mut output_deinterleaved,
-                frames_read,
-                output_expected,
+            let input_frames = frames_read;
+            let output_frames_f = frames_read as f64 / speed;
+            let processed_frames = stream.process(
+                Some(&input_stream_buffer),
+                &mut output_stream_buffer,
+                input_frames,
+                output_frames_f,
                 pitch,
             );
 
             // fetch stream's latency after the first process call
-            let pending_latency_frames =
-                *latency_frames.get_or_insert_with(|| (stream.latency() as f64 / speed) as usize);
+            let remaining_frames_to_skip = *remaining_latency_frames
+                .get_or_insert_with(|| (stream.latency() as f64 / speed) as usize);
 
             // skip empty latency buffers with the first process calls
-            let frames_to_skip = pending_latency_frames.min(output_frame_count);
-            latency_frames.replace(pending_latency_frames - frames_to_skip);
+            let frames_to_skip = remaining_frames_to_skip.min(processed_frames);
+            remaining_latency_frames.replace(remaining_frames_to_skip - frames_to_skip);
 
-            // push result to interleaved i16 sample buffer
-            for i in frames_to_skip..output_frame_count {
-                for ch in 0..num_channels {
-                    let sample = output_deinterleaved[ch][i];
-                    wav_output_samples.push((sample * 32767.0) as i16);
-                }
-            }
+            // write result to interleaved sample buffer vector
+            wav_write(&output_stream_buffer, frames_to_skip, processed_frames);
         }
-
-        total_frames_read += frames_read;
 
         if frames_read < input_block_size {
             break; // End of input file
@@ -114,31 +124,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // flush remaining output samples
+    let mut remaining_input_frames = stream.latency().ceil() as usize;
     loop {
-        let output_expected = output_block_size_f;
-        let output_frame_count = stream.process(
+        // process with mute input
+        let input_frames = remaining_input_frames.min(input_block_size);
+        let output_frames_f = input_frames as f64 / speed;
+        let processed_frames = stream.process(
             None,
-            &mut output_deinterleaved,
-            input_block_size,
-            output_expected,
+            &mut output_stream_buffer,
+            input_frames,
+            output_frames_f,
             pitch,
         );
 
-        let remaining_frames = total_frames_read.saturating_sub(stream.output_position() as usize);
-        if remaining_frames == 0 {
-            break;
-        }
+        // write result to interleaved sample buffer vector
+        wav_write(&output_stream_buffer, 0, processed_frames);
 
-        for i in 0..output_frame_count.min(remaining_frames) {
-            for ch in 0..num_channels {
-                let sample = output_deinterleaved[ch][i];
-                wav_output_samples.push((sample * 32767.0) as i16);
-            }
+        remaining_input_frames = remaining_input_frames.saturating_sub(input_frames);
+        if remaining_input_frames == 0 {
+            break; // End of output file
         }
     }
 
     // Write Wav output file
-    write(
+    wavers::write(
         &output_path,
         &wav_output_samples,
         sample_rate as i32,
